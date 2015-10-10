b55a518fc62cce9c8cd44b724ae85ed0ca6ffee5
elcinerkin <elcinerkin@gmail.com>
2015-10-10 13:41:29 -0700

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name noteeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the noteeApp
+ */
+angular.module('noteeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
